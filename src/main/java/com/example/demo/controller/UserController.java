445d518e493d9f522d9e951e04477c5df74445c1
guoyanrui <wb_guoyanrui@kuaishou.com>
2021-08-20 12:31:32 +0800

    add view

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,53 +3,62 @@ package com.example.demo.controller;
 import com.example.demo.bean.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * @author guoyanrui on 2021/8/17
  */
-@RestController
+@Controller
+@RequestMapping("/my")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @RequestMapping("/getUser")
+    @ResponseBody
     public List<User> getStaging() {
         return userService.getAllUser();
     }
+
     @RequestMapping("/add")
+    @ResponseBody
     public String addUser(@RequestParam(value = "id") int id,
-                        @RequestParam(value = "name") String name,
-                        @RequestParam(value = "password") String password) {
+                          @RequestParam(value = "name") String name,
+                          @RequestParam(value = "password") String password) {
 
         int res = userService.insert(id, name, password);
         if (res > 0) {
             return "增加成功";
         } else {
-           return "插入失败";
+            return "插入失败";
         }
     }
+
     @RequestMapping("/delete")
-    public String del(@RequestParam(value = "id") int id){
-        int  res = userService.deleteUser(id);
-        if(res>0){
+    @ResponseBody
+    public String del(@RequestParam(value = "id") int id) {
+        int res = userService.deleteUser(id);
+        if (res > 0) {
             return "删除成功";
-        }else{
+        } else {
             return "删除失败";
         }
     }
+
     @RequestMapping("/getUserById")
+    @ResponseBody
     public User getUserId(@RequestParam(value = "id") int id) {
         return userService.selectOne(id);
     }
+
     @RequestMapping("/update")
+    @ResponseBody
     public String updateUser(@RequestParam(value = "id") int id,
-                          @RequestParam(value = "name") String name,
-                          @RequestParam(value = "password") String password) {
+                             @RequestParam(value = "name") String name,
+                             @RequestParam(value = "password") String password) {
 
         int res = userService.update(id, name, password);
         if (res > 0) {
@@ -58,8 +67,8 @@ public class UserController {
             return "更新失败";
         }
     }
-    @RequestMapping("/main")
-    public String getmain(  ){
+    @RequestMapping(value = "/main",method = RequestMethod.GET)
+    public String getmain() {
         return "index";
     }
 }
